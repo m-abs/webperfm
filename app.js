f8cf410136f0d403d57825e8bf02f92f55342980
Morten Sjøgren <m_abs@mabs.dk>
2014-01-19 01:05:31 +0100

    Add GPLv2 license

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+/*!
+ ***********************************************************************
+webprefm - performance test tool for webservices
+Copyright (C) 2014 Morten Anton Bach Sjøgren <m_abs@mabs.dk>
+
+This file is part of webprefm.
+
+Foobar is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Foobar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+***********************************************************************/
 /*jshint node: true */
 
 /*global console: false */
