f9273a0f3ebf6357914d7d007d7e891f3def3eb7
Morten Sjøgren <m_abs@mabs.dk>
2014-02-22 22:18:56 +0100

    Foobar => webperfm

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ Copyright (C) 2014 Morten Anton Bach Sjøgren <m_abs@mabs.dk>
 
 This file is part of webprefm.
 
-Foobar is free software: you can redistribute it and/or modify
+webprefm is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 (at your option) any later version.
 
-Foobar is distributed in the hope that it will be useful,
+webprefm is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+along with webprefm.  If not, see <http://www.gnu.org/licenses/>.
 ***********************************************************************/
 /*jshint node: true */
 
